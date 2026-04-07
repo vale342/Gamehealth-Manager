5b5d62a04fcb3383ff69fa62980f7a5a51e6c59e
vale342 <valetrujillo126@gmail.com>
2026-04-07 17:06:54 -0600

    Modo oscuro y enlaces entre pantallas

--- app/src/main/java/com/example/gamehealthmanager/LoginFragment.kt
+++ app/src/main/java/com/example/gamehealthmanager/LoginFragment.kt
@@ -21,14 +21,14 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         binding.tvRegisterAction2.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment2)
-
+        }
+        binding.tvRegisterAction.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_fragment_recuperarContra)
         }
 
         return binding.root
     }
     /*Se declaro diferentes binding con _ es una variable y la otra una constante
     Se usará la constante binding para acceder a los elementos de la interfaz de usuario
-    ? Significa que puede ser opcional (en memoria el null no significa naa y el vacio si puede estar asiggnado, pero no contiene valor)
-
-    */
+    ? Significa que puede ser opcional (en memoria el null no significa naa y el vacio si puede estar asiggnado, pero no contiene valor)    */
 }
--- app/src/main/java/com/example/gamehealthmanager/MainActivity.kt
+++ app/src/main/java/com/example/gamehealthmanager/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.gamehealthmanager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
