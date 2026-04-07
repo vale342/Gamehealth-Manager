f605d021e0fb86bf3ddf1c7903df899e906d29c2
vale342 <valetrujillo126@gmail.com>
2026-04-07 16:27:54 -0600

    Diseño de fragmentos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -42,6 +46,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/gamehealthmanager/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.gamehealthmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.gamehealthmanager.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.tvRegisterAction2.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment2)
+
+        }
+
+        return binding.root
+    }
+    /*Se declaro diferentes binding con _ es una variable y la otra una constante
+    Se usará la constante binding para acceder a los elementos de la interfaz de usuario
+    ? Significa que puede ser opcional (en memoria el null no significa naa y el vacio si puede estar asiggnado, pero no contiene valor)
+
+    */
+}
